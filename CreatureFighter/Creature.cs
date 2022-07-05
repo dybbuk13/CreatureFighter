02d65a3fb2e62315da34e6e6fcff1d5cded1fdc0
santabomb5 <santabomb5@JUNKRAT>
2022-07-05 11:03:41 -0500

    Add project files.

---
+++ CreatureFighter/Creature.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatureFighter
+{
+    internal class Creature
+    {
+        private int health = GetRandomHealth();
+        public string creatureName;
+        private bool isAlive = true;
+
+        public Creature(string name)
+        {
+            this.creatureName = name;
+        }
+
+        public static void AttackCreature(string name, List<Creature> creatures)
+        {
+            Creature attackedCreature;
+            int attackDamage = GetRandomAttack();
+            foreach (Creature creature in creatures)
+            {
+                if(creature.creatureName == name && creature.isAlive != false)
+                {
+                    creature.health -= attackDamage;
+                    Console.WriteLine($"You successfully dealt { attackDamage }.");
+                    if (creature.health <= 0)
+                    {
+                        creature.isAlive = false;
+                        Console.WriteLine($"Congrats! You killed {name}!");
+                    }
+                    return;
+                }
+            }
+            Console.WriteLine($"{name} was not found.");
+        }
+
+        public static void ListAllCreatures(List<Creature> creatures)
+        {
+            creatures.ForEach(x => Console.WriteLine(x.creatureName));
+        }
+
+        public static void ListAliveCreatures(List<Creature> creatures)
+        {
+            List<Creature> aliveCreatures = new List<Creature>();
+            foreach (Creature c in creatures)
+            {
+                if (c.isAlive != false)
+                {
+                    aliveCreatures.Add(c);
+                }
+            }
+            aliveCreatures.ForEach(x => Console.WriteLine(x.creatureName));
+        }
+
+        private static int GetRandomAttack()
+        {
+            Random random = new Random();
+            int randomAttack = random.Next(0, 50);
+
+            return randomAttack;
+        }
+
+        private static int GetRandomHealth()
+        {
+            Random rollNumber = new Random();
+            int randomNumber = rollNumber.Next(1, 10) * 10;
+
+            return randomNumber;
+        }
+    }
+
+    
+}
---
+++ CreatureFighter/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace CreatureFighter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Creature> creatures = new List<Creature>();
+
+            Console.WriteLine("Hello, please enter a command.");
+            Console.WriteLine("(current commands: 'Create', 'List', 'Quit')");
+
+            bool menuCondition = true;
+            string cmd = "";
+            string nameInput = "";
+
+            while (menuCondition)
+            {
+                cmd = Console.ReadLine().ToUpper();
+
+                switch (cmd)
+                {
+                    case "CREATE":
+                        Console.Write("Please enter the creature's name: ");
+                        nameInput = Console.ReadLine();
+                        creatures.Add(new Creature(nameInput));
+                        Console.WriteLine($"{ nameInput } was created!");
+                        break;
+
+                    case "ATTACK":
+                        Console.WriteLine("Which creature would you like to attack?");
+                        Creature.ListAliveCreatures(creatures);
+                        nameInput = Console.ReadLine();
+                        Creature.AttackCreature(nameInput, creatures);
+                        break;
+
+                    case "LIST":
+                        Creature.ListAllCreatures(creatures);
+                        break;
+
+                    case "QUIT":
+                        menuCondition = false;
+                        break;
+                }
+                Console.Write("Please enter another command: ");
+            }
+
+            //Console.WriteLine("Hello, let's start by creating a few creatures.");
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.Write($"Please enter a name for Creature {i}:");
+            //    nameInput = Console.ReadLine();
+            //    creatures.Add(new Creature(nameInput));
+            //}
+
+            //Creature.AttackCreature("Lock", creatures);
+        }
+    }
+}
